0ba35792c09097588713f2595066ae25a0fbf721
yewon0219 <123551974+yewon0219@users.noreply.github.com>
2023-10-25 21:06:02 +0900

    FEAT: 메뉴바 각 버튼 Link태그 사용
    메인에서, 이미지를 불러와서 메뉴바의 버튼들을 구현하였던 것에서 이미지 제거 후, Link태그 이용해 텍스트와 링크로 직접 구현했습니다.

--- life-map/src/NavBar.js
+++ life-map/src/NavBar.js
@@ -1,40 +1,26 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
 import './NavBar.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import {Link} from 'react-router-dom';
 
 function NavBar() {
+  
   return (
-    <Navbar className="Navbar">
-      <Container>
-        <Navbar.Brand href="./Main.js" className='Lifemap'>
-          <img src={require("./img/LIFEMAP.png")}></img>
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#Question" className='Question'>
-            <img src={require("./img/QUESTION.png")}></img>
-            </Nav.Link>
-            <Nav.Link href="#BucketList" className='Bucketlist'>
-            <img src={require("./img/BUCKETLIST.png")}></img>
-            </Nav.Link>
-            <Nav.Link href="#Community" className='Community'>
-            <img src={require("./img/COMMUNITY.png")}></img>
-            </Nav.Link>
-          </Nav>
-          <Nav>
-            <Nav.Link href="#SignIn" className='Signin'>
-            <img src={require("./img/로그인.png")}></img>
-            </Nav.Link>
-            <Nav.Link href="/SignUp" className='Signup'>
-            <img src={require("./img/회원가입.png")} ></img>
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <div className='Navbar'>
+      <Link className="Lifemap" to={'/Main'}>
+        <span className='Purple'>L</span>
+        IFE
+        <span className='Purple'>M</span>
+        AP
+        </Link>
+      <Link className='Question' to={'/Question'}>QUESTION</Link>
+      <Link className='Bucketlist' to={'/BucketList'}>BUCKETLIST</Link>
+      <Link className='Community' to={'/Community'}>COMMUNITY</Link>
+      <Link className='Signin' to={'/SignIn'}>
+      <button type="button" class="btn btn-dark">SIGN IN</button>
+      </Link>
+      <Link className='Signup' to={'/SignUp'}>
+      <button type="button" class="btn btn-dark">SIGN UP</button>
+      </Link>
+    </div>
   );
 }
 
