773d7f3183ff738362eb0d6ef4420b2f50b32e8e
yewon0219 <123551974+yewon0219@users.noreply.github.com>
2023-11-17 21:38:32 +0900

    FEAT: 마이페이지 레이아웃 구현
    마이페이지 버튼 클릭 시 마이페이지로 이동하고, 레이아웃 구현까지 완료했습니다.

--- life-map/src/pages/MyPage.js
+++ life-map/src/pages/MyPage.js
@@ -1,9 +1,41 @@
+import { useState } from 'react';
 import NavBar from './NavBar.js';
+import image from '../img/프로필.png';
+
 function MyPage(){
+
+    const [nickname, setNickName] = useState("");
+    const [status, setStatus] = useState("");
+
+    const nicknameChangeHandler = (e) => {
+        setNickName(e.target.value);
+    }
+
+    const statusChangeHandler = (e) => {
+        setStatus(e.target.value);
+    }
+
+    const SubmitHandler = (e) => {
+        try {
+
+        } catch (error) {
+        
+        }
+      };
+
     return (
         <div className="MyPage">
             <NavBar/>
-            <div className='Sign-bg'>
+            <div className='myPage-bg'>
+                <div className='myPage-box'>
+                    <div className='myPage-box-title'>MY PAGE</div>
+                    <div className='myPage-box-image'><img src={image}></img></div>
+                    <div className='myPage-box-nickname'>NICKNAME</div>
+                    <input type='text' value={nickname} onChange={nicknameChangeHandler} placeholder="  YOUR NICKNAME" className='myPage-box-nickname-input'></input>
+                    <div className='myPage-box-status'>STATUS MESSAGE</div>
+                    <input type='text' value={status} onChange={statusChangeHandler} placeholder="  Enter your status message" className='myPage-box-status-input'></input>
+                    <button type="button" onClick={SubmitHandler} className='myPage-box-button'>SUBMIT</button>
+                </div>
             </div>
         </div>
     );
