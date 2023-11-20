8826156aaac7455a029b412a74278857f1a60916
yewon0219 <123551974+yewon0219@users.noreply.github.com>
2023-11-20 20:25:57 +0900

    FEAT: 네브바 선택 시 색상 변경 및 마이페이지에 알림창 추가
    NavBar에서 버튼 선택 시 페이지로 이동하며 색상이 변경되고, 마이페이지의 닉네임 및 상태메시지 입력창 제거 후 알림창을 스크롤뷰로 생성하였습니다.

--- life-map/src/pages/MyPage.js
+++ life-map/src/pages/MyPage.js
@@ -5,25 +5,7 @@ import { getAuth, signOut } from 'firebase/auth';
 
 function MyPage(){
 
-    const [nickname, setNickName] = useState("");
-    const [status, setStatus] = useState("");
     const item=localStorage.getItem("key");
-
-    const nicknameChangeHandler = (e) => {
-        setNickName(e.target.value);
-    }
-
-    const statusChangeHandler = (e) => {
-        setStatus(e.target.value);
-    }
-
-    const SubmitHandler = (e) => {
-        try {
-
-        } catch (error) {
-        
-        }
-      };
 
       async function SignOut() {
         try {
@@ -45,11 +27,41 @@ function MyPage(){
                     <div className='myPage-box-title'>MY PAGE</div>
                     <div className='myPage-box-image'><img src={image}></img></div>
                     <div className='myPage-box-id'>{item}</div>
-                    <div className='myPage-box-nickname'>NICKNAME</div>
-                    <input type='text' value={nickname} onChange={nicknameChangeHandler} placeholder="  YOUR NICKNAME" className='myPage-box-nickname-input'></input>
-                    <div className='myPage-box-status'>STATUS MESSAGE</div>
-                    <input type='text' value={status} onChange={statusChangeHandler} placeholder="  Enter your status message" className='myPage-box-status-input'></input>
-                    <button type="button" onClick={SubmitHandler} className='myPage-box-button'>SUBMIT</button>
+                    <div className='notification-title'>NOTIFICATION</div>
+                    <div className='notification' style={{ maxHeight: '230px', width: '500px', overflowY: 'scroll' }}>
+                        <ul>
+                            <li className='list'>▷(2023.11.10) 오늘의 질문이 도착했습니다!</li>
+                            <li className='list2'>▷(2023.11.10) abc@naver.com님께서 버킷리스트 '제주도 한달살이'에 좋아요를 누르셨습니다.</li>
+                            <li className='list2'>▷(2023.11.10) abc@naver.com님께서 버킷리스트 '제주도 한달살이'에 함께하고 싶어합니다.</li>
+                            <li className='list2'>▷(2023.11.10) qwer1234@gmail.com님께서 버킷리스트 '가족사진 100장 찍기'에 좋아요를 누르셨습니다.</li>
+                            <li className='list'>▷(2023.11.11) 오늘의 질문이 도착했습니다!</li>
+                            <li className='list2'>▷(2023.11.11) hihell00@naver.com님께서 나의 오늘의 답변에 공감했습니다.</li>
+                            <li className='list2'>▷(2023.11.11) 버킷리스트 '빼뺴로 데이에 부모님께 편지쓰기'를 달성하셨습니다! 축하드립니다.</li>
+                            <li className='list2'>▷(2023.11.11) 버킷리스트 '케이크 10조각 먹어보기'를 달성하셨습니다! 축하드립니다.</li>
+                            <li className='list'>▷(2023.11.12) 오늘의 질문이 도착했습니다!</li>
+                            <li className='list'>▷(2023.11.13) 오늘의 질문이 도착했습니다!</li>
+                            <li className='list2'>▷(2023.11.13) abc@naver.com님께서 커뮤니티 게시글 '케이크 10조각 먹어보기 꿀팁 드립니다~'에 댓글을 다셨습니다.</li>
+                            <li className='list2'>▷(2023.11.13) yeyeye22@naver.com님께서 '7일만에 리액트 마스터' 버킷리스트 함께하기 신청을 수락하셨습니다.</li>
+                            <li className='list'>▷(2023.11.14) 오늘의 질문이 도착했습니다!</li>
+                            <li className='list2'>▷(2023.11.14) qwer1234@gmail.com님께서 버킷리스트 '한성대 주변 맛집 정복'에 좋아요를 누르셨습니다.</li>
+                            <li className='list2'>▷(2023.11.14) 버킷리스트 '가족사진 100장 찍기'가 가장 많은 좋아요를 받았습니다! 달성하는 그날까지 화이팅!</li>
+                            <li className='list'>▷(2023.11.15) 오늘의 질문이 도착했습니다!</li>
+                            <li className='list'>▷(2023.11.16) 오늘의 질문이 도착했습니다!</li>
+                            <li className='list'>▷(2023.11.17) 오늘의 질문이 도착했습니다!</li>
+                            <li className='list2'>▷(2023.11.17) abc@naver.com님께서 커뮤니티 게시글 '수다떨어용~~!!'에 댓글을 다셨습니다.</li>
+                            <li className='list2'>▷(2023.11.17) zzzzz098765@naver.com님께서 커뮤니티 게시글 '수다떨어용~~!!'에 댓글을 다셨습니다.</li>
+                            <li className='list'>▷(2023.11.18) 오늘의 질문이 도착했습니다!</li>
+                            <li className='list'>▷(2023.11.19) 오늘의 질문이 도착했습니다!</li>
+                            <li className='list2'>▷(2023.11.19) 10293847@gmail.com님께서 커뮤니티 게시글 '수다떨어용~~!!'의 내 댓글에 답글을 다셨습니다.</li>
+                            <li className='list'>▷(2023.11.20) 오늘의 질문이 도착했습니다!</li>
+                            <li className='list2'>▷(2023.11.20) 버킷리스트 '7일만에 리액트 마스터'를 yeyeye22@naver.com님과 함께 달성 완료하셨습니다! 축하드립니다.</li>
+                            <li className='list'>▷(2023.11.21) 오늘의 질문이 도착했습니다!</li>
+                            <li className='list'>▷(2023.11.22) 오늘의 질문이 도착했습니다!</li>
+                            <li className='list2'>▷(2023.11.22) catdoglove@naver.com님께서 나의 오늘의 답변에 공감했습니다.</li>
+                            <li className='list'>▷(2023.11.23) 오늘의 질문이 도착했습니다!</li>
+                        </ul>
+                    </div>
+                    
                     <button type="button" className='SignOut' onClick={SignOut}>SIGN OUT</button>
                     
                 </div>
--- life-map/src/pages/NavBar.js
+++ life-map/src/pages/NavBar.js
@@ -1,10 +1,30 @@
 import '../css/NavBar.css';
-import {Link} from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import {Link, useLocation} from 'react-router-dom';
 import image from '../img/프로필.png';
 
 function NavBar() {
+  const [selectedTab, setSelectedTab] = useState('main');
+  const location = useLocation();
   const item=localStorage.getItem("key");
   
+  const handleTabClick = (tab) => {
+    setSelectedTab(tab);
+  };
+
+  useEffect(() => {
+    const currentPath = location.pathname;
+
+    if (currentPath.startsWith('/question')) {
+      setSelectedTab('question');
+    } else if (currentPath.startsWith('/bucket-list')) {
+      setSelectedTab('bucket-list');
+    } else if (currentPath.startsWith('/community')) {
+      setSelectedTab('community');
+    }
+  
+  }, [location.pathname]);
+
   return (
     <div className='Navbar'>
       <Link className="Lifemap" to={'/main'}>
@@ -13,9 +33,9 @@ function NavBar() {
         <span className='Purple'>M</span>
         AP
         </Link>
-      <Link className='Question' to={'/question'}>QUESTION</Link>
-      <Link className='Bucketlist' to={'/bucket-list'}>BUCKETLIST</Link>
-      <Link className='Community' to={'/community/bucket_community'}>COMMUNITY</Link>
+      <Link className={`Question ${selectedTab === 'question' ? 'selected' : ''}`} to={'/question'} onClick={() => handleTabClick('question')}>QUESTION</Link>
+      <Link className={`Bucketlist ${selectedTab === 'bucket-list' ? 'selected' : ''}`} to={'/bucket-list'} onClick={() => handleTabClick('bucket-list')}>BUCKETLIST</Link>
+      <Link className={`Community ${selectedTab === 'community' ? 'selected' : ''}`} to={'/community/bucket_community'} onClick={() => handleTabClick('community')}>COMMUNITY</Link>
       { item ? (
         <>
         <Link to={'/my-page'}>
